6691501ec76490514c5fbc86bbfb53ba8a7f119a
williamdowden <90417984+williamdowden@users.noreply.github.com>
2022-10-25 18:47:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include <vector>
 #include "GradeBook.h"
+using namespace std;
 
 int main() {
+    
+    // call GradeBook constructor
+    GradeBook gradebook();
+    gradebook.finalGrade(grades);
     return 0;
 }
